794d02d0a3fa820392c8ad8e2470967668899e49
Rutul Shah <rutulshah2007@gmail.com>
2020-01-15 00:09:20 +0530

    Added username and password inside Login form

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -5,6 +5,29 @@ import logo from '../../assets/logo.svg';
 import Modal from 'react-modal';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import Input from '@material-ui/core/Input';
+
+const customStyles = {
+    content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)'
+    }
+};
+
+const TabContainer = function (props) {
+    return (
+        <Typography component="div" style={{padding:0}}>
+            {props.children}
+        </Typography>
+    );
+}
 
 class Header extends Component {
 
@@ -39,11 +62,21 @@ class Header extends Component {
                 </Button>
             </div>
         </header>
-        <Modal ariaHideApp={false} isOpen={this.state.isModalOpen} contentLabel="Login" onRequestClose={this.closeModalHandler}>
+        <Modal style={customStyles} ariaHideApp={false} isOpen={this.state.isModalOpen} contentLabel="Login" onRequestClose={this.closeModalHandler}>
         <Tabs value={this.state.value} onChange={this.onChangeHandler}>
             <Tab label="LOGIN"></Tab>
             <Tab label="REGISTER"></Tab>
         </Tabs>
+        <TabContainer>
+                    <FormControl required>
+                        <InputLabel htmlFor="username"> Username </InputLabel>
+                        <Input id="username" type="text"/>
+                    </FormControl>
+                    <FormControl required>
+                        <InputLabel htmlFor="password"> Password </InputLabel>
+                        <Input id="password" type="password"/>
+                    </FormControl>
+        </TabContainer>
         </Modal>
         </div>
 
