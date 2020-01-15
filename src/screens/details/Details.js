792efbab284df2314d9ca376f9cd0c6f6b091e3f
Rutul Shah <rutulshah2007@gmail.com>
2020-01-15 23:15:17 +0530

    Added Back to Home button in details page

--- src/screens/details/Details.js
+++ src/screens/details/Details.js
@@ -3,6 +3,8 @@ import Header from '../../common/header/Header';
 import moviesData from '../../common/movieData';
 import './Details.css'
 import { Typography } from '@material-ui/core';
+import ReactDOM from 'react-dom';
+import Home from '../home/Home';
 
 class Details extends Component {
     constructor() {
@@ -20,11 +22,20 @@ class Details extends Component {
         this.setState({ currentState });
     }
 
+    backtohomeHandler = () => {
+        ReactDOM.render(<Home />, document.getElementById("root"));
+    }
+
     render() {
         let movie = this.state.movie;
         return (
             <div className="details">
                 <Header />
+                <div className="back">
+                    <Typography onClick={this.backtohomeHandler}>
+                        &#60; Back To Home
+                    </Typography>
+                </div><br/>
                 <div className="flex-containerDetails">
                     <div className="leftDetails">
                         <img src={movie.poster_url}></img>
@@ -34,7 +45,11 @@ class Details extends Component {
                             <Typography variant="headline" component="h2">{movie.title}</Typography>
                         </div>
                         <div>
-                            <Typography><span className="bold"> Genre: </span><span>{movie.genres.join(",")}</span></Typography>
+                            <Typography><span className="bold"> Genre: </span><span>{movie.genres.join(",")}</span></Typography><br/><br/>
+                            <Typography><span className="bold"> Duration: </span><span>{movie.duration}</span></Typography>
+                            <Typography><span className="bold"> Release Date: </span><span>{new Date(movie.release_date).toDateString()}}</span></Typography>
+                            <Typography><span className="bold"> Rating: </span><span>{movie.critics_rating}</span></Typography><br/>
+                            <Typography><span className="bold"> Plot: <a href={movie.wiki_url}> Wiki Link </a></span><span>{movie.storyline}</span></Typography>
                         </div>
                     </div>
                     <div className="rightDetails"></div>
