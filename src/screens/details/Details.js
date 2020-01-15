3f7f6881bb81dd84908222c68186e8236d6415ac
Rutul Shah <rutulshah2007@gmail.com>
2020-01-15 20:36:26 +0530

    Added poster, title and genres on details page

--- src/screens/details/Details.js
+++ src/screens/details/Details.js
@@ -1,31 +1,42 @@
 import React, { Component } from 'react';
 import Header from '../../common/header/Header';
 import moviesData from '../../common/movieData';
+import './Details.css'
+import { Typography } from '@material-ui/core';
 
 class Details extends Component {
     constructor() {
         super();
         this.state = {
-            movie:{}
+            movie: {}
         }
     }
 
-    componentDidMount() {
+    componentWillMount() {
         let currentState = this.state;
         currentState.movie = moviesData.filter((mov) => {
-           return mov.id === this.props.movieId;
-        });
-        this.setState({currentState});
-        console.log(currentState);
+            return mov.id === this.props.movieId;
+        })[0];
+        this.setState({ currentState });
     }
 
     render() {
+        let movie = this.state.movie;
         return (
             <div className="details">
                 <Header />
                 <div className="flex-containerDetails">
-                    <div className="leftDetails"></div>
-                    <div className="middleDetails"></div>
+                    <div className="leftDetails">
+                        <img src={movie.poster_url}></img>
+                    </div>
+                    <div className="middleDetails">
+                        <div>
+                            <Typography variant="headline" component="h2">{movie.title}</Typography>
+                        </div>
+                        <div>
+                            <Typography><span className="bold"> Genre: </span><span>{movie.genres.join(",")}</span></Typography>
+                        </div>
+                    </div>
                     <div className="rightDetails"></div>
                 </div>
             </div>
