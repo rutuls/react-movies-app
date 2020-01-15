3a6d6230a37af4bfe43ce39afdfb4f114815c0bd
Rutul Shah <rutulshah2007@gmail.com>
2020-01-15 12:29:53 +0530

    Added grid list with title bar

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -23,6 +23,10 @@ const styles = theme => ({
         flexWrap: 'nowrap',
         transform: 'translateZ(0)',
         width: '100%'
+    },
+    gridListMain: {
+        transform: 'translateZ(0)',
+        cursor: 'pointer'
     }
  });
 
@@ -39,12 +43,30 @@ const styles = theme => ({
                 <GridList cols={5} className={classes.gridListUpcomingMovies}>
                     {moviesData.map(movie => (
                         <GridListTile key={movie.id}>
-                            <img src={movie.poster_url} alt={movie.title}></img>
+                            <img src={movie.poster_url} alt={movie.title} className="movie-poster"></img>
                             <GridListTileBar title={movie.title}>
                             </GridListTileBar>
                         </GridListTile>
                     ))}
                 </GridList>
+
+                <div className="flex-container">
+                    <div className="left">
+                        <GridList cellHeight={350} cols={4} className={classes.gridListMain}>
+                            {moviesData.map(movie => (
+                                <GridListTile className="released-movie-grid-item" key={"grid" + movie.id}>
+                                    <img src={movie.poster_url} className="movie-poster" alt={movie.title} />
+                                    <GridListTileBar
+                                        title={movie.title}
+                                        subtitle={<span>Release Date: {new Date(movie.release_date).toDateString()}</span>}
+                                    />
+                                </GridListTile>
+                            ))}
+                        </GridList>
+                    </div>
+                    <div className="right">
+                    </div>
+                </div>
             </div>
         )
 
