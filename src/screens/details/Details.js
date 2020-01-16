0563fc8ac87abf22c8e7781f81cc175817674e1f
Rutul Shah <rutulshah2007@gmail.com>
2020-01-16 20:56:46 +0530

    Added youtube component for movie trailer

--- src/screens/details/Details.js
+++ src/screens/details/Details.js
@@ -5,6 +5,7 @@ import './Details.css'
 import { Typography } from '@material-ui/core';
 import ReactDOM from 'react-dom';
 import Home from '../home/Home';
+import YouTube from 'react-youtube';
 
 class Details extends Component {
     constructor() {
@@ -28,6 +29,13 @@ class Details extends Component {
 
     render() {
         let movie = this.state.movie;
+        let opts = {
+            height: "300",
+            width: "700",
+            playerVars:{
+                autoplay: 1
+            }
+        };
         return (
             <div className="details">
                 <Header />
@@ -35,7 +43,7 @@ class Details extends Component {
                     <Typography onClick={this.backtohomeHandler}>
                         &#60; Back To Home
                     </Typography>
-                </div><br/>
+                </div>
                 <div className="flex-containerDetails">
                     <div className="leftDetails">
                         <img src={movie.poster_url}></img>
@@ -45,11 +53,17 @@ class Details extends Component {
                             <Typography variant="headline" component="h2">{movie.title}</Typography>
                         </div>
                         <div>
-                            <Typography><span className="bold"> Genre: </span><span>{movie.genres.join(",")}</span></Typography><br/><br/>
+                            <Typography><span className="bold"> Genre: </span><span>{movie.genres.join(",")}</span></Typography>
                             <Typography><span className="bold"> Duration: </span><span>{movie.duration}</span></Typography>
                             <Typography><span className="bold"> Release Date: </span><span>{new Date(movie.release_date).toDateString()}}</span></Typography>
-                            <Typography><span className="bold"> Rating: </span><span>{movie.critics_rating}</span></Typography><br/>
+                            <Typography><span className="bold"> Rating: </span><span>{movie.critics_rating}</span></Typography>
                             <Typography><span className="bold"> Plot: <a href={movie.wiki_url}> Wiki Link </a></span><span>{movie.storyline}</span></Typography>
+                            <Typography className="trailerContainer"><span className="bold">Trailer:</span></Typography>
+                            <YouTube
+                            videoId={movie.trailer_url.split("?v=")[1]}
+                            opts={opts}
+                            onReady={this._onReady}
+                            ></YouTube>
                         </div>
                     </div>
                     <div className="rightDetails"></div>
